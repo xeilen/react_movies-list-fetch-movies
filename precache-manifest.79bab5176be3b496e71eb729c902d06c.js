129998a88747a0b40a6cab9f6b68a32a509c3162
38063 <xeilen1112@gmail.com>
2020-11-08 19:21:23 +0200

    Updates

---
+++ precache-manifest.79bab5176be3b496e71eb729c902d06c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f768780e72b9049694b2b32537ed7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "835aa092c93730b5876d",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "de785732e5c048c6d961",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "835aa092c93730b5876d",
+    "url": "./static/js/2.e8294957.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8294957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de785732e5c048c6d961",
+    "url": "./static/js/main.028c7820.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
